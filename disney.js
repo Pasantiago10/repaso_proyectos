eeee44060a506bf7d3411dc1f408de8b9a392a88
santi <santiagorojas772@gmail.com>
2025-04-08 11:57:46 -0500

    se creo el arreglo de peliculas disney con personajes que aparecen en los films, segun su id.

--- disney.js
+++ disney.js
@@ -2,7 +2,7 @@
 let url = 'https://api.disneyapi.dev/';
 
 async function obtianmovieimport(id) {
-    const character = [];
+    const charactersinfo = [];
     let films = [];
     
     let urlmi = `${url}character`;
@@ -15,9 +15,36 @@ async function obtianmovieimport(id) {
     //se trae a url con los parametros requeridos y los opcionales
        const response = await fetch(urlmi);
        //se pasa el respuesta a json
-           const data = await response.json();
-    
-         console.log(data);
+const data = await response.json();
+
+//se crea variable flist para convertir films que esta dentro de data a arreglo para que pueda ser leido con un for
+const flist =  data.data.films;
+
+//se recorre flist y se inserta films
+for(let i=o; i<flist.length; i++) {
+    films.push({"!films":flist[i]})
 }
+//se crea la variable character 
+const c = [];
+
+//se inserta status en el arrrelo c
+c.push({"name": data.data.name});
+c.push({"magen": data.data.imageUrl});
+c.push({"recent update": data.data.updatedAt})
+
+
+//se inseta c en charactersinfo
+charactersinfo.push({"characters information": c});
+
+
+// //solo si es u  arreglo interno se recorre
+
+//insertamos charactersinfor en films
+films.push({"characterinformation": charactersinfo});
+console.log(JSON.stringify(films));
+
+
+}
+
 //se llama la funcion
-obtianmovieimport(230);
+obtianmovieimport(24)
